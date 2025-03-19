76b28fa6c434a020605fd283e76b28e64a262060
BeefBoy <a.fokin1@yandex.com>
2025-03-19 12:57:56 +0300

    all buttons ready

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 
 from loguru import logger
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -6,8 +7,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DB_URL: str = "postgresql+asyncpg://postgres:1234@localhost:5432/test_chat"
-    SECRET_KEY: str = "123"
-    SESSION_KEY: str = "12345"
+    SECRET_KEY: str = "changethis"
+    SESSION_KEY: str = "changethis"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_ALIVE_TIME: int = 30
     REFRESH_TOKEN_ALIVE_TIME: int = 2880
@@ -18,6 +19,16 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env"))
 
 
+    def _check_default_secret(self, var_name: str, value: str | None) -> None:
+        if value == "changethis":
+            message = (
+                f'The value of {var_name} is "changethis", '
+                "for security, please change it, at least for deployments."
+            )
+            if self.ENVIRONMENT == "local":
+                warnings.warn(message, stacklevel=1)
+            else:
+                raise ValueError(message)
 # Получаем параметры для загрузки переменных среды
 settings = Settings()
 log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log.txt")
